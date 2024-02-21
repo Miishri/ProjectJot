1ef03006b0a1af4036e370b05b058122a9afa8a6
Kiwi <muhammadshamaeem@example.com>
2024-02-21 01:58:03 +0100

    initialize project

---
+++ src/main/java/org/personal/projectjot/ProjectJotApplication.java
@@ -0,0 +1,13 @@
+package org.personal.projectjot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectJotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectJotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/personal/projectjot/ProjectJotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.personal.projectjot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectJotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
