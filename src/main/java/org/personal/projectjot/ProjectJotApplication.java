f5b96cdf7a4f36e0e2bd50c38105617ed75fba5e
Kiwi <muhammadshamaeem@example.com>
2024-03-02 01:34:48 +0100

    reset security

--- src/main/java/org/personal/projectjot/ProjectJotApplication.java
+++ src/main/java/org/personal/projectjot/ProjectJotApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjectJotApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectJotApplication.class, args);
 	}
